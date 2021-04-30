bd9f723af5d782bf1750eeb89fe74394b9cbaf84
BalakaDEV <69608661+BalakaDEV@users.noreply.github.com>
2021-04-30 20:43:30 -0300

    Inventario

---
+++ blk_inventory/blkclient.lua
@@ -0,0 +1,257 @@
+local Tunnel = module("vrp","lib/Tunnel")
+local Proxy = module("vrp","lib/Proxy")
+vRP = Proxy.getInterface("vRP")
+
+--[ CONEXÃO ]----------------------------------------------------------------------------------------------------------------------------
+
+vRPNserver = Tunnel.getInterface("blk_inventory")
+
+vRPC = {}
+Tunnel.bindInterface("blk_inventory",vRPC)
+Proxy.addInterface("blk_inventory",vRPC)
+emP = Tunnel.getInterface("blk_inventory")
+
+--[ VARIÁVEIS ]--------------------------------------------------------------------------------------------------------------------------
+
+local invOpen = false
+local animacao = false 
+
+--[ STARTFOCUS ]-------------------------------------------------------------------------------------------------------------------------
+
+Citizen.CreateThread(function()
+	SetNuiFocus(false,false)
+end)
+
+--[ INVCLOSE ]---------------------------------------------------------------------------------------------------------------------------
+
+RegisterNUICallback("invClose",function(data)
+	StopScreenEffect("MenuMGSelectionIn")
+	SetCursorLocation(0.5,0.5)
+	SetNuiFocus(false,false)
+	SendNUIMessage({ action = "hideMenu" })
+	invOpen = false
+end)
+
+--[ ABRIR INVENTARIO ]-------------------------------------------------------------------------------------------------------------------
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(1)
+        local ped = PlayerPedId()
+        if IsControlJustPressed(0,243) then
+            if GetEntityHealth(ped) > 101 and not vRP.isHandcuffed() and not IsPedBeingStunned(ped) and not IsPlayerFreeAiming(ped) then
+                if not invOpen then
+                	StartScreenEffect("MenuMGSelectionIn", 0, true)
+                    invOpen = true
+                    SetNuiFocus(true,true)
+                    SendNUIMessage({ action = "showMenu" })
+                else
+                	StopScreenEffect("MenuMGSelectionIn")
+                    SetNuiFocus(false,false)
+                    SendNUIMessage({ action = "hideMenu" })
+                    invOpen = false
+                end
+            end
+        end
+    end
+end)
+
+--[ CLONEPLATES ]------------------------------------------------------------------------------------------------------------------------
+
+RegisterNetEvent('cloneplates')
+AddEventHandler('cloneplates',function()
+    local ped = PlayerPedId()
+    local vehicle = GetVehiclePedIsUsing(ped)
+    local clonada = GetVehicleNumberPlateText(vehicle)
+    if IsEntityAVehicle(vehicle) then
+        PlateIndex = GetVehicleNumberPlateText(vehicle)
+        SetVehicleNumberPlateText(vehicle,"CLONADA")
+        FreezeEntityPosition(vehicle,false)
+        if clonada == CLONADA then 
+            SetVehicleNumberPlateText(vehicle,PlateIndex)
+            PlateIndex = nil
+        end
+    end
+end)
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- REPAIRTIRES
+-----------------------------------------------------------------------------------------------------------------------------------------
+RegisterCommand("pneu",function(source,args) 
+    source = source 
+    local ped = PlayerPedId()
+    local currentPos = GetEntityCoords(ped)
+    local vehicle = vRP.getNearestVehicle(3) 
+    if IsPedInAnyVehicle(PlayerPedId(), false) then
+        TriggerEvent("Notify","negado","Você não pode trocar o pneu dentro de um <b>veículo</b>.")
+        vehicle = false 
+    end        
+        if vehicle then
+            animacao = false 
+            Desabilitar()
+            animacao = true 
+            vRP._playAnim(false,{{"anim@amb@clubhouse@tutorial@bkr_tut_ig3@","machinic_loop_mechandplayer"}},true)
+            TriggerEvent("progress",10000,"Trocando Pneu")
+            Wait(10000)
+            SetVehicleTyreFixed(vehicle,0,0)
+            SetVehicleTyreFixed(vehicle,1,1)
+            SetVehicleTyreFixed(vehicle,2,2)
+            SetVehicleTyreFixed(vehicle,3,3)
+            SetVehicleTyreFixed(vehicle,4,4)
+            SetVehicleTyreFixed(vehicle,5,5)
+            SetTimeout(10000,function()
+                vRP._stopAnim(source,false)
+            end)
+            animacao = false 
+        else
+        TriggerEvent("Notify","negado","Você precisa estar próximo de um <b>veículo</b>.")
+        end  
+    --end    
+end)
+
+function Desabilitar()
+    Citizen.CreateThread(function()
+        while true do
+            Citizen.Wait(1)
+            if animacao then
+                BlockWeaponWheelThisFrame()
+                DisableControlAction(0,167,true)
+                DisableControlAction(0,169,true)
+            end     
+        end
+    end)
+end
+--[ VEHICLEANCHOR ]----------------------------------------------------------------------------------------------------------------------
+
+RegisterNetEvent('vehicleanchor')
+AddEventHandler('vehicleanchor',function()
+    local ped = PlayerPedId()
+    local vehicle = GetVehiclePedIsUsing(ped)
+    FreezeEntityPosition(vehicle,true)
+end)
+
+--[ DROPITEM ]---------------------------------------------------------------------------------------------------------------------------
+
+RegisterNUICallback("droparItem",function(data)
+    local ped = PlayerPedId()
+    if IsPedInAnyVehicle(ped) then
+        TriggerEvent("Notify","negado","Você não pode dropar itens quando estiver em um veículo.")
+    else
+        vRPNserver.dropItem(data.item,data.type,data.amount)
+    end
+end)
+
+--[ SENDITEM ]---------------------------------------------------------------------------------------------------------------------------
+
+RegisterNUICallback("enviarItem",function(data)
+	vRPNserver.sendItem(data.item,data.type,data.amount)
+end)
+
+--[ USEITEM ]----------------------------------------------------------------------------------------------------------------------------
+
+RegisterNUICallback("usarItem",function(data)
+	vRPNserver.useItem(data.item,data.type,data.amount)
+end)
+
+--[ MOCHILA ]----------------------------------------------------------------------------------------------------------------------------
+
+RegisterNUICallback("requestMochila",function(data,cb)
+	local inventario,peso,maxpeso = vRPNserver.Mochila()
+	if inventario then
+		cb({ inventario = inventario, peso = peso, maxpeso = maxpeso })
+	end
+end)
+
+--[ AUTO-UPDATE ]------------------------------------------------------------------------------------------------------------------------
+
+RegisterNetEvent("vrp_inventario:Update")
+AddEventHandler("vrp_inventario:Update",function(action)
+	SendNUIMessage({ action = action })
+end)
+
+RegisterNetEvent("vrp_inventario:fechar")
+AddEventHandler("vrp_inventario:fechar",function(action)
+	StopScreenEffect("MenuMGSelectionIn")
+    SetNuiFocus(false,false)
+    SendNUIMessage({ action = "hideMenu" })
+    invOpen = false
+end)
+
+--[ USO REMÉDIO ]------------------------------------------------------------------------------------------------------------------------
+
+local usandoRemedios = false
+RegisterNetEvent("remedios")
+AddEventHandler("remedios",function()
+    local ped = PlayerPedId()
+    local health = GetEntityHealth(ped)
+    local armour = GetPedArmour(ped)
+
+    SetEntityHealth(ped,health)
+    SetPedArmour(ped,armour)
+    
+    if GetEntityHealth(ped) < 300 then
+        TriggerEvent("Notify","negado","<b>O remédio não fara feito, pois você precisa de tratamento</b>.",8000)
+    else
+        if usandoRemedios then
+            return
+        end
+
+        usandoRemedios = true
+
+        if usandoRemedios then
+            repeat
+                Citizen.Wait(600)
+                if GetEntityHealth(ped) > 230 then
+                    SetEntityHealth(ped,GetEntityHealth(ped)+1)
+                end
+            until GetEntityHealth(ped) >= 400 or GetEntityHealth(ped) <= 280
+                TriggerEvent("Notify","sucesso","O medicamento acabou de fazer efeito.",8000)
+                usandoRemedios = false
+        end
+    end
+end)
+
+--[ GMOCHILA ]---------------------------------------------------------------------------------------------------------------------------
+
+RegisterCommand("mochila",function(source,args)
+    if vRPNserver.checkMochila() then
+        if args[1] == "1" then
+            SetPedComponentVariation(PlayerPedId(),5,52,0,2) -- Deserto;
+        elseif args[1] == "2" then
+            SetPedComponentVariation(PlayerPedId(),5,52,1,2) -- Camuflada;
+        elseif args[1] == "3" then
+            SetPedComponentVariation(PlayerPedId(),5,52,2,2) -- Camuflada 2;
+        elseif args[1] == "4" then
+            SetPedComponentVariation(PlayerPedId(),5,52,3,2) -- Verde;
+        elseif args[1] == "5" then
+            SetPedComponentVariation(PlayerPedId(),5,52,4,2) -- Preta;
+        elseif args[1] == "6" then
+            SetPedComponentVariation(PlayerPedId(),5,52,5,2) -- Preta com azul;
+        elseif args[1] == "7" then
+            SetPedComponentVariation(PlayerPedId(),5,52,6,2) -- Preta com cinza;
+        elseif args[1] == "8" then
+            SetPedComponentVariation(PlayerPedId(),5,52,7,2) -- Preta com deserto;
+        elseif args[1] == "9" then
+            SetPedComponentVariation(PlayerPedId(),5,52,8,2) -- Preta com branco;
+        elseif args[1] == "10" then
+            SetPedComponentVariation(PlayerPedId(),5,52,9,2) -- Preta com preto;
+        end
+    end
+end)
+
+RegisterNetEvent("inventario:mochilaon")
+AddEventHandler("inventario:mochilaon",function()
+    SetPedComponentVariation(PlayerPedId(),5,52,4,2)
+end)
+
+RegisterNetEvent("inventario:mochilaoff")
+AddEventHandler("inventario:mochilaoff",function()
+    SetPedComponentVariation(PlayerPedId(),5,-1,0,2)
+end)
+
+function vRPC.checkVida()
+    local ped = PlayerPedId()
+    if GetEntityHealth(ped) >= 280 then
+        return true
+    end
+    return false
+end
---
+++ blk_inventory/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'adamant'
+game 'gta5'
+
+ui_page 'nui/darkside.html'
+
+Author 'Balaka#9918'
+
+client_scripts {
+	'@vrp/lib/utils.lua',
+	'blkclient.lua'
+}
+
+server_scripts {
+	'@vrp/lib/utils.lua',
+	'blkserver.lua'
+}
+
+files {
+	"nui/darkside.html",
+	"nui/lightsaber.js",
+	"nui/theforce.css",
+	"nui/images/*.png"
+}
